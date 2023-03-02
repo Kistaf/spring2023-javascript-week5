0bee1d245a8cc3b2fa7727e2650ca92e5937c17f
stkea <stef240p@stud.kea.dk>
2023-03-02 16:08:01 +0100

    fixes

--- day3/exercise-4/index.js
+++ day3/exercise-4/index.js
@@ -38,8 +38,8 @@ const showAllMembers = async () => {
     paragraph.innerHTML = htmlString;
 }
 
-const findMemberById = async (id) => {
-    const paragraph = document.getElementById('single-member-result')
+const findMemberById = async (id, paragraphId) => {
+    const paragraph = document.getElementById(paragraphId)
     const url = `http://localhost:8080/api/members/${id}`
     const { status, result } = await fetchData(url, "GET")
     if (status !== 200) {
@@ -56,7 +56,7 @@ document.getElementById('btn-get-all').onclick = showAllMembers
 document.getElementById('single-member-btn').onclick = () => {
     const input = document.getElementById("text-for-username")
     if (input.value === "") return;
-    findMemberById(input.value)
+    findMemberById(input.value, 'single-member-result')
 }
 
 document.getElementById('new-member-form').onsubmit = async (e) => {
@@ -77,13 +77,96 @@ document.getElementById('new-member-form').onsubmit = async (e) => {
     const url = `http://localhost:8080/api/members`
     const res = await fetch(url, {
         method: "POST",
-        body: body,
+        body: JSON.stringify(body),
         headers: {
             "Content-Type": "application/json"
         }
     })
-    console.log(await res.json())
+    const paragraph = document.getElementById('new-member-result')
+    if (res.status !== 200) {
+        paragraph.innerText = "Failed to create a new member"
+        return
+    }
+    const data = await res.json()
+    paragraph.innerText = `
+    Username: ${data.username}
+    Email: ${data.email}
+    `
+    await showAllMembers()
 }
 
+
+document.getElementById('fetch_car_to_edit_form').onsubmit = async (e) => {
+    e.preventDefault()
+    const form = document.getElementById('fetch_car_to_edit_form')
+    const inputField = form.edit_car_id
+    const update_form = document.getElementById('update_car_form')
+    if (inputField.value === "") return
+    const res = await fetch('http://localhost:8080/api/cars/' + inputField.value, {
+        method: "GET"
+    })
+    document.getElementById('updated').innerText = ''
+    if (res.status !== 200) {
+        update_form.innerText = "Failed to fetch the car"
+        return
+    }
+    const data = await res.json()
+    update_form.innerHTML = `
+        <input hidden id="id" value=${data.id} />
+        <div><label for="brand">Brand</label><input id="brand" value=${data.brand} /></div>
+        <div><label for="model">Model</label><input id="model" value=${data.model} /></div>
+        <div><label for="pricePrDay">Price Pr Day</label><input id="pricePrDay" value=${data.pricePrDay} /></div>
+        <button>Change</button>
+    `
+}
+
+document.getElementById('update_car_form').onsubmit = async (e) => {
+    e.preventDefault()
+    const form = document.getElementById('update_car_form')
+    const id = form.id
+    const brand = form.brand
+    const model = form.model
+    const pricePrDay = form.pricePrDay
+    const body = {
+        id: id.value,
+        brand: brand.value,
+        model: model.value,
+        pricePrDay: pricePrDay.value
+    }
+    const res = await fetch('http://localhost:8080/api/cars', {
+        method: "PUT",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const showResult = document.getElementById('updated')
+    if (res.status !== 200) {
+        showResult.innerText = "Failed to update values"
+        return
+    }
+    const data = await res.json()
+    form.innerHTML = ''
+    showResult.innerHTML = `
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Price Pr Day</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>${data.id}</td>
+                <td>${data.brand}</td>
+                <td>${data.model}</td>
+                <td>${data.pricePrDay}</td>
+            </tr>
+        </tbody>
+    </table>
+    `
+}
 
 
